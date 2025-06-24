093d3f280b0c2d587a58d19bbbbd411c163542a3
Matheeo <90444801+Matheeo@users.noreply.github.com>
2025-06-24 15:59:25 +0200

    Gestion logs

--- simulation/simulation_setter.py
+++ simulation/simulation_setter.py
@@ -9,7 +9,7 @@ def accidents_generator(blocked_vehicles: dict[str, int], step: int):
     for vehicule in vehicules_ids:
         if traci.vehicle.getTypeID(vehicule) == "veh__private":
             if random.randint(1, 1000) == 1:
-                logger.info(f"Accident generated for vehicle {vehicule} at step {step}")
+                logger.debug(f"Accident generated for vehicle {vehicule} at step {step}")
                 traci.vehicle.setSpeed(vehicule, 0)
                 blocked_vehicles[vehicule] = step + 10
 
@@ -17,6 +17,6 @@ def accidents_liberator(blocked_vehicles: dict[str, int], step: int):
 
     for vehicule, step_liberator in list(blocked_vehicles.items()):
         if step_liberator == step:
-            logger.info(f"Accident liberated for vehicle {vehicule} at step {step}")
+            logger.debug(f"Accident liberated for vehicle {vehicule} at step {step}")
             del blocked_vehicles[vehicule]
             traci.vehicle.setSpeed(vehicule, -1)
--- util/logger.py
+++ util/logger.py
@@ -3,5 +3,5 @@ import logging
 logger = logging.getLogger(__name__)
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
 )
